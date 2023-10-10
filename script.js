d0e6b93e14ac3967a7fbafb3db84f2e86c04b3bc
Huzaifa0718 <146505580+Huzaifa0718@users.noreply.github.com>
2023-10-10 06:51:29 +0000

    changed

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 // function add(){
 let a = document.getElementById("amount")
-  let e = document.getElementById("balance")
+let e = document.getElementById("balance")
 let b = document.getElementById("text")
 
-  let c = document.getElementById("income")
-  let d = document.getElementById("expense")
-  let h = document.getElementById("add")
-  let u = document.getElementById("deduct")
-  let z = document.getElementById("net")
-  let x = document.getElementById("daalo")
+let c = document.getElementById("income")
+let d = document.getElementById("expense")
+let h = document.getElementById("add")
+let u = document.getElementById("deduct")
+let z = document.getElementById("net")
+let x = document.getElementById("daalo")
 
 let balance = 0;
-let income =0;
-let expense =0;
+let income = 0;
+let expense = 0;
 
-h.addEventListener("click",()=>{
-   e.innerHTML = a.value
-  let newbalance =parseInt(e.innerHTML)+balance;
-e.innerHTML = newbalance;
+h.addEventListener("click", () => {
+  e.innerHTML = a.value
+  let newbalance = parseInt(e.innerHTML) + balance;
+  e.innerHTML = newbalance;
   console.log(newbalance)
-   balance = newbalance ;
+  balance = newbalance;
 })
 
-u.addEventListener("click",()=>{
-   d.innerHTML = a.value;
-    let newexpense =parseInt(d.innerHTML)+expense;
-d.innerHTML = newexpense;
+u.addEventListener("click", () => {
+  d.innerHTML = a.value;
+  let newexpense = parseInt(d.innerHTML) + expense;
+  d.innerHTML = newexpense;
   console.log(newexpense)
-   expense = newexpense ;
+  expense = newexpense;
 })
 
 // function net(){
@@ -41,19 +41,21 @@ d.innerHTML = newexpense;
 //     e.innerHTML = balance-d.innerHTML
 // }
 
-z.addEventListener("click",()=>{
-  e.innerHTML = balance-d.innerHTML
+z.addEventListener("click", () => {
+  e.innerHTML = balance - d.innerHTML
 })
 
-function text()
-{ 
-  if(b.value === ''){
-  alert("kuch likh bhai")
-}else{let li = document.createElement("li");
- li.innerHTML  = b.value
-document.getElementById("list").appendChild(li)
-  let span = document.createElement("span");
-  span.innerHTML = a.value;
-li.appendChild(span);}
+function text() {
+  if (b.value === '') {
+    alert("kuch likh bhai")
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = b.value
+    document.getElementById("list").appendChild(li)
+    let span = document.createElement("span");
+    li.classList.add('styled-li');
+    span.innerHTML = a.value;
+    li.appendChild(span);
+  }
   b.value = "";
 }
